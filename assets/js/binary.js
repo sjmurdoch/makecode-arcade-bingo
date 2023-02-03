5b0da0b4831e23c1857cc58307270ad294955a41
Steven Murdoch <s.murdoch@ucl.ac.uk>
2023-02-03 21:07:06 +0000

    0.1.3

--- assets/js/binary.js
+++ assets/js/binary.js
@@ -1,4 +1,4 @@
-// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.12.18/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"0.1.2","target":"arcade","targetVersion":"1.12.18","repo":"sjmurdoch/makecode-arcade-bingo"}
+// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.12.18/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"0.1.3","target":"arcade","targetVersion":"1.12.18","repo":"sjmurdoch/makecode-arcade-bingo"}
 // total=1334668 new=6.39% cached=88.17% other=5.44%
 (function (ectx) {
 'use strict';
@@ -430,7 +430,7 @@ const pxsim_pxtrt = pxsim.pxtrt;
 const pxsim_numops = pxsim.numops;
 
 
-function _main___P56959(s) {
+function _main___P57034(s) {
 let r0 = s.r0, step = s.pc;
 s.pc = -1;
 
@@ -974,13 +974,13 @@ switch (step) {
     return leave(s, r0)
   default: oops()
 } } }
-_main___P56959.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
-_main___P56959.continuations = [  ]
+_main___P57034.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
+_main___P57034.continuations = [  ]
 
-function _main___P56959_mk(s) {
+function _main___P57034_mk(s) {
     checkStack(s.depth);
     return {
-        parent: s, fn: _main___P56959, depth: s.depth + 1,
+        parent: s, fn: _main___P57034, depth: s.depth + 1,
         pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
   tmp_0: undefined,
   tmp_1: undefined,
@@ -45200,5 +45200,5 @@ const sprites_BaseSpriteSayRenderer__C40204_VT = mkVTable({
 
 const breakpoints = setupDebugger(1, ["seq___42273","shown___42274","bigFont___42275","medFont___42276","allNumbers___42278","background___42283","current___42282","showingAll___42277","lastN___42281","last___42280"])
 
-return _main___P56959
+return _main___P57034
 })
