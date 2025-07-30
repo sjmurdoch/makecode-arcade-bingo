842be5186b66e48c9e19356c22e29f5fe020a960
Steven Murdoch <s.murdoch@ucl.ac.uk>
2025-07-30 21:37:29 +0100

    Update pxt.json, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,27 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "song1":
+            case "mySong":return hex`0078000408020100001c00010a006400f401640000040000000000000000000000000005000004060000000400012c`;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
